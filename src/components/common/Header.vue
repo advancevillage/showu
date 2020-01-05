f01db06f1217014ea77cf2d3321d8d5c6428f65e
advancevillage <cugriver@gmail.com>
2020-01-05 22:57:34 +0800

    #richard: 顶部通知栏和顶部导航栏合并

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -11,6 +11,35 @@
                 </p>
             </div>
         </nav>
+        <!-- 菜单页 -->
+        <div class="container_menu">
+            <b-navbar class="container_menu_items" :class="{'container_menu_items_hover': containerMenu.hover}" v-on:mouseenter.native="containerMenuItemsMouseEnter()" v-on:mouseleave.native="containerMenuItemsMouseLeave()">
+                <template slot="start">
+                    <b-navbar-item :key="index" href="#" v-for="(cat, index) in containerMenu.categories"
+                                   v-on:mouseenter.native="containerMenuItemMouseEnter(index)"
+                                   v-on:mouseleave.native="containerMenuItemMouseLeave()">
+                        <span class="container_menu_item" :class="{'container_menu_item_hover': showContainerMenuItem(index) }">{{cat.categoryName}}</span>
+                    </b-navbar-item>
+                </template>
+                <template slot="end">
+                    <b-navbar-item href="#" >
+                        <span class="container_menu_item">{{containerMenu.user.userNickName}}</span>
+                    </b-navbar-item>
+                    <b-navbar-item href="#" >
+                        <b-button class="container_menu_item_cart">{{containerMenu.user.userCartCount}}</b-button>
+                    </b-navbar-item>
+                </template>
+            </b-navbar>
+            <div class="container_menu_item_context" :class="{'container_menu_item_context_show': showContainerMenuItemContext() }"  v-on:mouseenter="containerMenuItemContextMouseEnter()" v-on:mouseleave="containerMenuItemContextMouseLeave()">
+                <div class="container_menu_item_context_list">
+                    <div class="container_menu_item_context_list_item" v-for="(item, index) in containerMenu.itemContext.list" :key="index">
+                        {{item.categories}}
+                    </div>
+                </div>
+                <div class="container_menu_item_context_images">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -22,10 +51,70 @@
                 selectItems: [
                     {id: "country", url: "/", value: "CN"},
                     {id: "themes", url: "/", value: ""},
-                ]
+                ],
+                containerMenu: {
+                    position: "is-left",
+                    size: "is-medium",
+                    categories: [
+                        {categoryName: "NewIn", categoryId: "1111111111"},
+                        {categoryName: "NewIn", categoryId: "2222222222"},
+                        {categoryName: "NewIn", categoryId: "3333333333"},
+                    ],
+                    hover: false,
+                    item: {
+                        hover: -1
+                    },
+                    itemContext: {
+                        hover: -1,
+                        list: [
+                            {categories: [1, 2, 3]},
+                            {categories: [4, 5, 6]},
+                            {categories: [7, 8 ,9]}
+                        ]
+                    },
+                    user: {
+                        userNickName: "richard",
+                        userId: "11111",
+                        userCartCount: 3
+                    },
+                },
             }
         },
-        name: "Header"
+        name: "Header",
+        methods: {
+            getCarouselImageUrl: function (value) {
+                return `https://picsum.photos/id/43${value}/1230/500`
+            },
+            /*菜单页*/
+            containerMenuItemsMouseEnter() {
+                this.containerMenu.hover = true;
+            },
+            containerMenuItemsMouseLeave() {
+                this.containerMenu.hover = false;
+            },
+            containerMenuItemMouseEnter(index) {
+                this.containerMenu.item.hover = index;
+                this.containerMenu.itemContext.hover = index;
+            },
+            containerMenuItemMouseLeave() {
+                this.containerMenu.item.hover = -1;
+            },
+            containerMenuItemContextMouseEnter() {
+                this.containerMenu.hover = true;
+                this.containerMenu.item.hover = this.containerMenu.itemContext.hover;
+            },
+            containerMenuItemContextMouseLeave() {
+                this.containerMenu.hover = false;
+                this.containerMenu.item.hover = -1;
+                this.containerMenu.itemContext.hover = -1;
+            },
+            showContainerMenuItem(index) {
+                return this.containerMenu.hover && this.containerMenu.item.hover === index
+            },
+            showContainerMenuItemContext() {
+                return this.containerMenu.hover && this.containerMenu.item.hover !== -1
+            }
+        }
     }
 </script>
 
@@ -36,8 +125,12 @@
         background: black;
         z-index: 25;
     }
+    /* 通知顶部栏 */
+    .level {
+        margin: 0;
+    }
     .level-item {
-        padding: 6px 0;
+        padding: 0.5% 0;
     }
     .header_notice_text {
         color: white;
@@ -48,5 +141,58 @@
         color: white;
         text-align: center;
         margin: 0 10px;
+    }
+    /* 顶部导航栏 */
+    .container_menu_items {
+        padding: 0 2%;
+        background: rgba(192, 192, 192, 0);
+    }
+    .container_menu_items_hover {
+        background: rgba(192, 192, 192, 1);
+    }
+    .container_menu_item {
+        width: 100%;
+        text-align: center;
+        color: white;
+    }
+    .container_menu_item_hover {
+        font-weight: bolder;
+        border-bottom: 2px solid white;
+    }
+    .container_menu_item_cart {
+        background-color: darkgray;
+        border-radius: 50px;
+    }
+    .container_menu_item_context {
+        width: 100%;
+        height: 240px;
+        padding: 0 2%;
+        display: none;
+        position: absolute;
+        z-index: 25;
+    }
+    .container_menu_item_context_show {
+        display: block;
+        background: rgba(216, 216, 216, 0.9);
+        border-top: 2px solid white;
+    }
+    .container_menu_item_context_list {
+        float: left;
+        width: 45%;
+        height: 100%;
+    }
+    .container_menu_item_context_list_item {
+        width: 25%;
+        height: 100%;
+        float: left;
+        margin: 1%;
+    }
+    .container_menu_item_context_images {
+        float: left;
+        width: 55%;
+        height: 100%;
+    }
+    a.navbar-item:focus, a.navbar-item:focus-within, a.navbar-item:hover {
+        background-color: rgba(128, 128, 128, 0);
     }
 </style>
--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -3,35 +3,6 @@
         <Header/>
         <div id="container">
             <div>
-                <!-- 菜单页 -->
-                <div class="container_menu">
-                    <b-navbar class="container_menu_items" :class="{'container_menu_items_hover': containerMenu.hover}" v-on:mouseenter.native="containerMenuItemsMouseEnter()" v-on:mouseleave.native="containerMenuItemsMouseLeave()">
-                        <template slot="start">
-                            <b-navbar-item :key="index" href="#" v-for="(cat, index) in containerMenu.categories"
-                                           v-on:mouseenter.native="containerMenuItemMouseEnter(index)"
-                                           v-on:mouseleave.native="containerMenuItemMouseLeave()">
-                                <span class="container_menu_item" :class="{'container_menu_item_hover': showContainerMenuItem(index) }">{{cat.categoryName}}</span>
-                            </b-navbar-item>
-                        </template>
-                        <template slot="end">
-                            <b-navbar-item href="#" >
-                                <span class="container_menu_item">{{containerMenu.user.userNickName}}</span>
-                            </b-navbar-item>
-                            <b-navbar-item href="#" >
-                                <b-button class="container_menu_item_cart">{{containerMenu.user.userCartCount}}</b-button>
-                            </b-navbar-item>
-                        </template>
-                    </b-navbar>
-                    <div class="container_menu_item_context" :class="{'container_menu_item_context_show': showContainerMenuItemContext() }"  v-on:mouseenter="containerMenuItemContextMouseEnter()" v-on:mouseleave="containerMenuItemContextMouseLeave()">
-                            <div class="container_menu_item_context_list">
-                                <div class="container_menu_item_context_list_item" v-for="(item, index) in containerMenu.itemContext.list" :key="index">
-                                    {{item.categories}}
-                                </div>
-                            </div>
-                            <div class="container_menu_item_context_images">
-                            </div>
-                    </div>
-                </div>
                 <div class="container_notes">
                     <div class="container_notes_top">
                     </div>
@@ -140,33 +111,6 @@
                     animated: 'fate',
                     pauseHover: true
                 },
-                containerMenu: {
-                    position: "is-left",
-                    size: "is-medium",
-                    categories: [
-                        {categoryName: "NewIn", categoryId: "1111111111"},
-                        {categoryName: "NewIn", categoryId: "2222222222"},
-                        {categoryName: "NewIn", categoryId: "3333333333"},
-                    ],
-                    hover: false,
-                    item: {
-                        hover: -1
-                    },
-                    itemContext: {
-                        hover: -1,
-                        list: [
-                            {categories: [1, 2, 3]},
-                            {categories: [4, 5, 6]},
-                            {categories: [7, 8 ,9]}
-                        ]
-                    },
-                    user: {
-                        userNickName: "richard",
-                        userId: "11111",
-                        userCartCount: 3
-                    },
-
-                },
                 containerNewIn: {
                     newIns: [
                         {imageUrl: "https://res.cloudinary.com/everlane/image/upload/c_fill,dpr_2.0,f_auto,h_580,q_auto,w_580/v1/i/f9bb5b3f_54e4.jpg", goodsId: "0000000000", goodsDescription: "Lorem ipsum dolor sit amet"},
@@ -226,42 +170,11 @@
             Header,
             Footer
         },
-        mounted() {
-            window.addEventListener('scroll',this.handleScroll, true)
-        },
         methods: {
             getCarouselImageUrl: function (value) {
                 return `https://picsum.photos/id/43${value}/1230/500`
             },
             /*菜单页*/
-            containerMenuItemsMouseEnter() {
-                this.containerMenu.hover = true;
-            },
-            containerMenuItemsMouseLeave() {
-                this.containerMenu.hover = false;
-            },
-            containerMenuItemMouseEnter(index) {
-                this.containerMenu.item.hover = index;
-                this.containerMenu.itemContext.hover = index;
-            },
-            containerMenuItemMouseLeave() {
-                this.containerMenu.item.hover = -1;
-            },
-            containerMenuItemContextMouseEnter() {
-                this.containerMenu.hover = true;
-                this.containerMenu.item.hover = this.containerMenu.itemContext.hover;
-            },
-            containerMenuItemContextMouseLeave() {
-                this.containerMenu.hover = false;
-                this.containerMenu.item.hover = -1;
-                this.containerMenu.itemContext.hover = -1;
-            },
-            showContainerMenuItem(index) {
-                return this.containerMenu.hover && this.containerMenu.item.hover === index
-            },
-            showContainerMenuItemContext() {
-                return this.containerMenu.hover && this.containerMenu.item.hover !== -1
-            }
         }
     }
 </script>
@@ -277,63 +190,6 @@
         min-height: 680px;
         width: 100%;
         height: 100%;
-    }
-    /*菜单页 菜单栏*/
-    .container_menu {
-        z-index: 25;
-        position: absolute;
-        width: 100%;
-        height: 40px;
-    }
-    .container_menu_items {
-        padding: 5px 40px 0 40px;
-        background: rgba(192, 192, 192, 0);
-    }
-    .container_menu_items_hover {
-        background: rgba(192, 192, 192, 1);
-    }
-    .container_menu_item {
-        width: 100%;
-        text-align: center;
-        color: white;
-    }
-    .container_menu_item_hover {
-        font-weight: bolder;
-        border-bottom: 2px solid white;
-    }
-    .container_menu_item_cart {
-        background-color: darkgray;
-        border-radius: 50px;
-    }
-    .container_menu_item_context {
-        width: 100%;
-        height: 240px;
-        padding: 0 40px;
-        display: none;
-    }
-    .container_menu_item_context_show {
-        display: block;
-        background: rgba(216, 216, 216, 0.9);
-        border-top: 2px solid white;
-    }
-    .container_menu_item_context_list {
-        float: left;
-        width: 45%;
-        height: 240px;
-    }
-    .container_menu_item_context_list_item {
-        width: 25%;
-        height: 220px;
-        float: left;
-        margin: 10px;
-    }
-    .container_menu_item_context_images {
-        float: left;
-        width: 55%;
-        height: 240px;
-    }
-    a.navbar-item:focus, a.navbar-item:focus-within, a.navbar-item:hover {
-        background-color: rgba(128, 128, 128, 0);
     }
     /*菜单栏 文字描述*/
     .container_notes {
